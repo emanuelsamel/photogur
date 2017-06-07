af53384977d8fec6115cec70d0773097a3e0a3ad
emanuelsamel <mannys94@hotmail.com>
2017-06-07 18:22:51 -0400

    text

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,4 +9,12 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
     # render :show  # render the view called show.html.erb
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+render text: "Received POST request to '/pictures' with the data URL: #{params}"
+  end
 end
