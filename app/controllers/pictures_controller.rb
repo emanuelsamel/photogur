23ab96458e805f88b61b47800631dc5ed72b2aaa
emanuelsamel <mannys94@hotmail.com>
2017-06-07 18:12:55 -0400

    show individual

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,5 +2,11 @@ class PicturesController < ApplicationController
 
   def index
     @pictures = Picture.all
+
+  end
+
+  def show
+    @picture = Picture.find(params[:id])
+    # render :show  # render the view called show.html.erb
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
+
 Rails.application.routes.draw do
-
   get 'pictures' => 'pictures#index'
-
-
+  get 'pictures/:id' => 'pictures#show'
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
