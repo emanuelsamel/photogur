3f3b7ca0c53616e7ccd7bdff00c4f5b56f753d33
emanuelsamel <mannys94@hotmail.com>
2017-06-19 12:43:35 -0400

    added user controller user loggin and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   patch 'pictures/:id' => "pictures#update"
 
   delete 'pictures/:id' => 'pictures#destroy'
+
+  resources :users
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
